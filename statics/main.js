cb07a3ed7f3848feb796cf0a9fc65370a607700e
Gonzalo <gonzalo.alfacaso@gmail.com>
2018-06-28 15:06:12 -0500

    frontend

---
+++ statics/main.js
@@ -0,0 +1,85 @@
+canvas = document.createElement('canvas');
+canvas.setAttribute('width', '100px');
+canvas.setAttribute('height', '100px');
+canvas.setAttribute('id', 'canvas');
+
+var canvasDiv = document.getElementById('canvasDiv');
+canvasDiv.appendChild(canvas);
+if(typeof G_vmlCanvasManager != 'undefined') {
+	canvas = G_vmlCanvasManager.initElement(canvas);
+}
+context = canvas.getContext("2d");
+
+$('#canvas').mousedown(function(e){
+  var mouseX = e.pageX - this.offsetLeft;
+  var mouseY = e.pageY - this.offsetTop;
+		
+  paint = true;
+  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+  redraw();
+});
+
+$('#canvas').mousemove(function(e){
+  if(paint){
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+    redraw();
+  }
+});
+
+$('#canvas').mouseup(function(e){
+  paint = false;
+});
+
+$('#canvas').mouseleave(function(e){
+  paint = false;
+});
+
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+
+function addClick(x, y, dragging)
+{
+  clickX.push(x);
+  clickY.push(y);
+  clickDrag.push(dragging);
+}
+
+function redraw(){
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+  
+  context.strokeStyle = "#000";
+  context.lineJoin = "round";
+  context.lineWidth = 5;
+			
+  for(var i=0; i < clickX.length; i++) {		
+    context.beginPath();
+    if(clickDrag[i] && i){
+      context.moveTo(clickX[i-1], clickY[i-1]);
+     }else{
+       context.moveTo(clickX[i]-1, clickY[i]);
+     }
+     context.lineTo(clickX[i], clickY[i]);
+     context.closePath();
+     context.stroke();
+  }
+}
+
+var imgData = context.getImageData(0, 0, context.canvas.width, context.canvas.height)
+console.log(imgData)
+var data = imgData.data
+$.ajax({
+    url: '/predict-digit',
+    data: JSON.stringify(data),
+    contentType: 'application/json; charset=utf-8',
+    type: 'POST',
+    success: function(response){
+        // response['digit']
+        // response['prob']
+    },
+    error: function(e){
+        console.log(e)
+    }
+})
+
